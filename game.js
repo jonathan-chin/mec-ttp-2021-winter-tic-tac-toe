6fdb8dfb5a40ef01678c5cb7bf4620d5b08d2821
Jon Chin <jon@sharebyteaching.org>
2021-01-08 15:27:28 -0500

    improved

--- game.js
+++ game.js
@@ -6,10 +6,25 @@ let board = [
     [' ', ' ', ' ']
 ];
 
+let wins = 0;
+let losses = 0;
+let ties = 0;
+
 function draw_board(){
     for(let i = 0; i < 3; i++){
 	for(let j = 0; j < 3; j++){
-	    document.getElementById(`tile-${i}-${j}`).innerHTML = board[i][j];
+	    let tile = document.getElementById(`tile-${i}-${j}`);
+	    let marker = board[i][j];
+
+	    // not optimized
+	    // unnecessarily modifying classes at every render
+	    // but ok for now
+	    if(marker == ' '){
+		tile.classList.remove('clicked');
+	    }else{
+		tile.classList.add('clicked');
+	    }
+	    tile.innerHTML = marker;
 	}
     }
 }
@@ -17,19 +32,41 @@ function draw_board(){
 function choose(x, y){
     let tile = board[x][y];
     if(tile != ' '){
-	alert('not allowed');
+	// do nothing
+	// rely on css to provide user visual feedback
     }else{
 	// assume the player is always X
 	board[x][y] = 'X';
 	draw_board();
-	let game_should_end = check_for_wlt(board);
-	if(game_should_end){
-	    // update scores
-	    // reset board
-	}else{
+	const result = check_for_wlt(board);
+	if(result == 'continue'){
 	    ai_turn();
+	}else{
+	    alert(result);
+	    switch(result){
+		case 'win':
+		    wins++;
+		    break;
+		case 'tie':
+		    ties++;
+		    break;
+	    }
+	    update_record();
 	}
     }
+}
+
+function update_record(){
+    document.getElementById('record').innerHTML = `wins: ${wins}, losses: ${losses}, ties: ${ties}`;
+}
+
+function reset(){
+    board = [
+	[' ', ' ', ' '],
+	[' ', ' ', ' '],
+	[' ', ' ', ' ']
+    ];
+    draw_board();
 }
 
 function ai_turn(){
@@ -39,12 +76,29 @@ function ai_turn(){
 	    if(board[i][j] == ' '){
 		board[i][j] = 'O';
 		draw_board();
-		return;
+		const result = check_for_wlt(board);
+		if(result == 'continue'){
+		    // switch turns back to user
+		    return;
+		}else{
+		    alert(result);
+		    switch(result){
+			case 'loss':
+			    losses++;
+			    break;
+			case 'tie':
+			    ties++;
+			    break;
+		    }
+		    update_record();
+		    return;
+		}
 	    }
 	}
     }
-    
 }
 
 
+// run these functions for the first time to set up the game
 draw_board();
+update_record();
--- wlt_logic.js
+++ wlt_logic.js
@@ -1,7 +1,91 @@
+// returns 'win', 'loss', 'tie', or 'continue'
 function check_for_wlt(board){
-    // assume for now no wlt
-    return false;
-    
-    // return true;
-    // return false;
+
+    // assume player is always X
+    if(
+	// horizontal checks
+	(board[0][0] == 'X' &&
+       board[0][1] == 'X' &&
+       board[0][2] == 'X') ||
+       (board[1][0] == 'X' &&
+       board[1][1] == 'X' &&
+       board[1][2] == 'X') || 
+       (board[2][0] == 'X' &&
+	board[2][1] == 'X' &&
+	board[2][2] == 'X') ||
+
+	// vertical checks
+	(board[0][0] == 'X' &&
+       board[1][0] == 'X' &&
+       board[2][0] == 'X') ||
+       (board[0][1] == 'X' &&
+       board[1][1] == 'X' &&
+       board[2][1] == 'X') || 
+       (board[0][2] == 'X' &&
+	board[1][2] == 'X' &&
+	board[2][2] == 'X') ||
+
+	// cross checks
+
+	(board[0][0] == 'X' &&
+	 board[1][1] == 'X' &&
+	 board[2][2] == 'X') ||
+	(board[0][2] == 'X' &&
+	 board[1][1] == 'X' &&
+	 board[2][0] == 'X')
+	){
+	    return 'win';
+    }
+
+
+        // assume ai is always O
+    if(
+	// horizontal checks
+	(board[0][0] == 'O' &&
+       board[0][1] == 'O' &&
+       board[0][2] == 'O') ||
+       (board[1][0] == 'O' &&
+       board[1][1] == 'O' &&
+       board[1][2] == 'O') || 
+       (board[2][0] == 'O' &&
+	board[2][1] == 'O' &&
+	board[2][2] == 'O') ||
+
+	// vertical checks
+	(board[0][0] == 'O' &&
+       board[1][0] == 'O' &&
+       board[2][0] == 'O') ||
+       (board[0][1] == 'O' &&
+       board[1][1] == 'O' &&
+       board[2][1] == 'O') || 
+       (board[0][2] == 'O' &&
+	board[1][2] == 'O' &&
+	board[2][2] == 'O') ||
+
+	// cross checks
+
+	(board[0][0] == 'O' &&
+	 board[1][1] == 'O' &&
+	 board[2][2] == 'O') ||
+	(board[0][2] == 'O' &&
+	 board[1][1] == 'O' &&
+	 board[2][0] == 'O')
+    ){
+	alert('loss');
+	    return 'loss';
+    }
+
+    // check if board is full
+    for(let i = 0; i < 3; i++){
+	for(let j = 0; j < 3; j++){
+	    console.log(board[i][j]);
+	    if(board[i][j] == ' '){
+		// there's at least one blank space
+		return 'continue';
+	    }
+	}
+    }
+
+    // player has not won, loss, or continue, so tie by default
+    return 'tie';
 }
